55a9255dbc7da654f6a92f40688b3ed0486c80b4
MrAndre2020 <ninfetahot123@gmail.com>
2021-02-22 14:45:32 -0300

    Add playstoreCommand

---
+++ src/commands/Pesquisas/playstoreCommand.js
@@ -0,0 +1,46 @@
+const Discord = require('discord.js');
+const moment = require("moment")
+const PlayStore = require("google-play-scraper");
+const EmbedColor = ``;
+moment.locale('pt-br')
+
+module.exports.run = async (client, message, args) => {
+    if (!args[0])
+      return message.channel.send(`:x: | Você precisa escrever o nome de um aplicativo.`);
+
+    PlayStore.search({
+      term: args.join(" "),
+      num: 1
+    }).then(Data => {
+      let App;
+
+      try {
+        App = JSON.parse(JSON.stringify(Data[0]));
+      } catch (error) {
+        return message.channel.send(
+          `:x: | Não consegui encontrar o aplicativo, desculpe.`
+        );
+      }
+
+      let Embed = new Discord.MessageEmbed()
+        .setColor(EmbedColor || "#3030E2")
+        .setThumbnail(App.icon)
+        .setURL(App.url)
+        .setTitle(`${App.title}`)
+        .setDescription(App.summary)
+        .addField(`Preço:`, App.priceText.replace('FREE', 'Gratis'), true)
+        .addField(`Desenvolvedor:`, App.developer, true)
+        .addField(`Avaliação:`, App.scoreText, true)
+        .setFooter(`${message.author.username}`)
+        .setTimestamp();
+
+      return message.channel.send(Embed);
+    });
+  };
+
+exports.help = {
+    name: 'playstore',
+    aliases: [],
+    categoria: 'Pesquisas',
+
+  }
